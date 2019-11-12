b84395eab210f96f094c3b57629237b6207444f1
Cristiane <cristiane.trc@gmail.com>
2019-11-12 09:48:18 -0300

    Atualização access token

--- instafeed.js
+++ instafeed.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
 
     get: 'user',
     userId: 8411780163,
-    accessToken: '8411780163.aeeff59.d2604cb0ac724e0c8d71957e0edce600',
+    accessToken: '8411780163.1677ed0.a79587546f4d44938e26a004c8747d9a',
     template: '<div><img src="{{image}}" title="{{caption}}"/></div>',
     target: 'instafeed',
     resolution: 'standard_resolution',
@@ -15,24 +15,24 @@ var feed = new Instafeed({
             autoplay: true,
             autoplaySpeed: 5000,
             responsive: [{
-                    breakpoint: 1025,
-                    settings: {
-                        slidesToShow: 6,
-                        slidesToScroll: 6
-                    }
-                }, {
-                    breakpoint: 601,
-                    settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 4
-                    }
-                }, {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                }]
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6
+                }
+            }, {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }]
         });
     }
 });
